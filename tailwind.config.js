16b58f8c1f327f6c782e66fdd615dcabe07590f9
Jan Lajara <janlajara@gmail.com>
2022-07-04 09:18:51 +0800

    add files to be purged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./public/index.html', './src/**/*.{css,vue,js,ts,jsx,tsx}'],
   content: [
     "./public/index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
